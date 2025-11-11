d02b13fc70e2811b89648c34388f305bdbb2b1f6
taewonKang1 <edward0317@naver.com>
2025-11-11 15:39:13 +0900

    feat: merging

--- sharestaybackend/src/main/java/com/example/sharestay/controller/UserController.java
+++ sharestaybackend/src/main/java/com/example/sharestay/controller/UserController.java
@@ -7,7 +7,6 @@ import com.example.sharestay.dto.UserProfileResponse;
 import com.example.sharestay.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
